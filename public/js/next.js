e3d13d161cb57bba7624a20c314cef1f00a4ebb5
Di.Wu <di.wu@homecreditcfc.cn>
2020-12-23 08:59:33 +0800

    新增当当前页面视频看完后，切换到下一个推荐未看过的视频功能

--- public/js/next.js
+++ public/js/next.js
@@ -1,5 +1,36 @@
 setInterval(() => {
+    let listTry = $('._3vlDUtmf button');
+    if (listTry.length !== 0) {
+        if (listTry.children('span').text() === 'Try again') {
+            listTry[0].click();
+        }
+    }
+
     let list = $('.player-modal-overlay.is-active > div > div > button');
-    if (list.length === 0) return;
-    list[0].click();
+    if (list.length === 0) {
+        console.log('length zero.');
+        return;
+    }
+
+    if (list[0].innerHTML.toString().indexOf('Continue to next module') !== -1) {
+        list[0].click();
+    } else if (list[0].innerHTML.toString().indexOf('View related courses') !== -1) {
+        goNextCourse().then(() => {
+        });
+    }
+
+
 }, 3000);
+
+async function goNextCourse() {
+    try {
+        const {recommendedCourses} = await fetch("/learner/user/courses/recommended", {
+            method: "GET"
+        }).then(response => response.json());
+
+        const {playerUrl} = recommendedCourses[0];
+
+        window.location.href = playerUrl
+    } catch (e) {
+    }
+}
