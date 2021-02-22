c517c19a2d487e726a29ee8328d1255cce1177d0
Di.Wu <di.wu@homecreditcfc.cn>
2021-02-22 13:16:08 +0800

    修复切换答题问题产生的bug

--- public/js/intro/changelocation.js
+++ public/js/intro/changelocation.js
@@ -1,5 +1,9 @@
-let location_url = window.location.href;
+setInterval(3000, () => {
+    let location_url = window.location.href;
 
-location_url = location_url.replace('score/skill-assessment', 'skilliq');
+    if (location_url.indexOf('/intro') === -1) {
+        location_url = location_url.replace('score/skill-assessment', 'skilliq');
 
-window.location.href = location_url;
+        window.location.href = location_url;
+    }
+});
